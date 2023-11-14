7f479c660c1d6be735029fa41ed87991e188583d
Umer237 <umerafzal981@gmail.com>
2023-11-14 18:16:25 +0500

    umer

---
+++ database/src/app/Confident/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Confident = () => {
+  return (
+    <div className='Login-Signup-Page-BG'>
+    <img className='Logo' src="./img/Logo.png" alt="" />
+    <div className='Login-Signup-Page-Button'>
+    <h3>WELCOME</h3>
+        <button><a href="/Login">Login</a></button>
+        <button><a href="/Signup">Sign up</a></button>        
+    </div>
+    <div className='Login-Signup-Page-Texts'>
+        <p>Limited Time Offer. Get Scaling & Polishing By Hygienist For Just £75. Incl. Book Now. Book Online Your Consultation Now Or Call Us Now. Friendly Dental Team</p>
+        <h3>www.confidentdp.com</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Confident;

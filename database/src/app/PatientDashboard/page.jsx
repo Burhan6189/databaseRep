34f7badac96e9393ed781e95098f60d76811cec9
Umer237 <umerafzal981@gmail.com>
2023-11-18 13:57:12 +0500

    umer

--- database/src/app/PatientDashboard/page.jsx
+++ database/src/app/PatientDashboard/page.jsx
@@ -1,51 +1,44 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 
 const PatientDashboard = () => {
-
-
-  const myfun=async()=>{
+  const myfun = async () => {
     alert("new table addeed");
-  }
-
+  };
 
   const inputArr = [
     {
       type: "text",
       id: 1,
-      value: ""
-    }
+      value: "",
+    },
   ];
 
   const [arr, setArr] = useState(inputArr);
 
   const addInput = () => {
-    setArr(s => {
+    setArr((s) => {
       return [
         ...s,
         {
           type: "text",
-          value: ""
-        }
+          value: "",
+        },
       ];
     });
   };
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     e.preventDefault();
 
     const index = e.target.id;
-    setArr(s => {
+    setArr((s) => {
       const newArr = s.slice();
       newArr[index].value = e.target.value;
 
       return newArr;
-    });}
-
-
-
-
-
+    });
+  };
 
   return (
     <>
@@ -222,7 +215,7 @@ const PatientDashboard = () => {
             <a href="/TreatmentPlan">Treatment Plan</a>
           </button>
           <div className="Details-Inputs">
-            <div>
+            {/* <div>
               <input className="side-input" type="text" />
               <input className="center-input" type="text" />
               <input className="side-input" type="text" />
@@ -231,33 +224,38 @@ const PatientDashboard = () => {
               <input className="side-input" type="text" />
               <input className="center-input" type="text" />
               <input className="side-input" type="text" />
-            </div>
-
-          
-           
+            </div> */}
+            {arr.map((item, i) => {
+            return (
+              <div className="Details-Inputs">
+                <input
+                  className="side-input"
+                  type={item.type}
+                  id={i}
+                  size="40"
+                />
+                <input
+                  className="center-input"
+                  type={item.type}
+                  id={i}
+                  size="40"
+                />
+                <input
+                  className="side-input"
+                  type={item.type}
+                  id={i}
+                  size="40"
+                />
+              </div>
+            );
+          })}
           </div>
         </div>
         <div>
-        
-         {arr.map((item, i) => {
-        return (
-         
-          <div className="Details-Inputs" >
-              <input className="side-input" type={item.type} id={i}
-            
-            size="40" />
-              <input className="center-input" type={item.type} id={i}
-        
-            size="40" />
-              <input className="side-input" type={item.type} id={i}
-            
-            size="40" />
-            </div>
-          
-
-        );
-      })} 
-       <button onClick={addInput} className="Add-Btn">+</button>
+      
+          <button onClick={addInput} className="Add-Btn">
+            +
+          </button>
         </div>
       </div>
     </>

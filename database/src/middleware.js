ae5f9d02affa97c4ccd85a008180e0d9f3e1e93a
Your Name <burhanshabbir6189@gmail.com>
2023-11-22 13:57:34 +0500

    burhan

--- database/src/middleware.js
+++ database/src/middleware.js
@@ -6,14 +6,14 @@ export function middleware(request) {
   let patientcookie = request.cookies.get("Patient");
 
   if (!patientcookie) {
-    if (request.nextUrl.pathname.startsWith("/Things") || request.nextUrl.pathname.startsWith("/PatientDashboard")) {
+    if (request.nextUrl.pathname.startsWith("/dashboard") ) {
       return NextResponse.rewrite(new URL("/Login", request.url));
     }
     
   } else {
     if (url.pathname === "/login") {
       if (patientcookie) {
-        url.pathname = "Things";
+        url.pathname = "dashboard";
 
         return NextResponse.redirect(url);
       }

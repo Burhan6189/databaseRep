b60a1983278b39f648bd24fd3d79b5a07ff7150b
Umer237 <umerafzal981@gmail.com>
2023-11-22 17:53:51 +0500

    umer

--- database/src/app/dashboard/page.jsx
+++ database/src/app/dashboard/page.jsx
@@ -8,56 +8,60 @@ const dashboard = () => {
           <img src="http://www.fhgroupoc.com/svg/fhlogog.svg" alt="" />
         </div>
         <div className="Box-Flex">
-
-
-        <a style={{textDecoration:"none"}} href="/dashboard/PatientDashboard">
-
-          <div className="Box-01" >
-            <div className="Icon-Circle">
-              <img src="/img/icon.png" alt="" />
+          <a
+            style={{ textDecoration: "none" }}
+            href="/dashboard/PatientDashboard"
+          >
+            <div className="Box-01">
+              <div className="Icon-Circle">
+                <img src="/img/icon01.png" alt="" />
+              </div>
+              <div className="Box-Text">
+                <h3>New Patient</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s,
+                </p>
+              </div>
             </div>
-            <div className="Box-Text">
-            <h3>New Patient</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s,
-            </p>
-            </div>
-          </div>
           </a>
 
-          <div className="Box-01">
+          <div className="Box-02">
             <div className="Icon-Circle">
-              <img src="/img/icon.png" alt="" />
+              <img src="/img/icon02.png" alt="" />
             </div>
             <div className="Box-Text">
-            <h3>Appointments</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s,
-            </p>
+              <h3>Appointment</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
             </div>
           </div>
 
-          <div className="Box-01">
+          <div className="Box-03">
             <div className="Icon-Circle">
-              <img src="/img/icon.png" alt="" />
+              <img src="/img/icon01.png" alt="" />
             </div>
             <div className="Box-Text">
-            <h3>Old Patient</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s,
-            </p>
+              <h3>Old Patient</h3>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s,
+              </p>
             </div>
           </div>
         </div>
         <div className="Bottom-Text">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        <h3>www.fhgroupoc.com</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s,
+          </p>
+          <h3>www.fhgroupoc.com</h3>
         </div>
       </div>
     </>

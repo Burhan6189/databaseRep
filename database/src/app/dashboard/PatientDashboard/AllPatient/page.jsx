3948b9af56fe4a367ec5cfb96349d37e37d69898
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-11-30 17:58:25 +0500

    umer

--- database/src/app/dashboard/PatientDashboard/AllPatient/page.jsx
+++ database/src/app/dashboard/PatientDashboard/AllPatient/page.jsx
@@ -1,35 +1,31 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
+import { HiOutlinePencilAlt } from "react-icons/hi";
 import Popup from "reactjs-popup";
 
-
 const AllPatient = () => {
+  const plusbtn = () => {
+    return (window.location.href = "/dashboard/PatientDashboard");
+  };
 
+  const [Details, setDetails] = useState([]);
 
-const plusbtn=()=>{
- return window.location.href=("/dashboard/PatientDashboard");
-}
+  const printdata = async () => {
+    const data = await fetch("/api/patientdetails");
+    const jsondata = await data.json();
 
+    setDetails(jsondata);
+  };
 
-const [Details, setDetails]=useState([]);
-
-const printdata = async ()=>{
-
-const data = await fetch("/api/patientdetails");
-const jsondata = await data.json();
-
-setDetails(jsondata);
-}
-
-useEffect(()=>{printdata(), []})
-
-
+  useEffect(() => {
+    printdata(), [];
+  });
 
   return (
     <>
-
       <div className="All-Patient-BG">
         <div className="Header">
           <div className="Header-Flex">
@@ -42,7 +38,7 @@ useEffect(()=>{printdata(), []})
                 src="http://www.fhgroupoc.com/svg/fhlogog.svg"
                 alt=""
               />
-            </div> 
+            </div>
             <div className="User-Login">
               <img className="User-Login-Pic" src="/img/image 2.png" alt="" />
               <div>
@@ -61,62 +57,58 @@ useEffect(()=>{printdata(), []})
           </div>
         </div>
 
-
-        
-        {
-     [ ...Details].reverse().map((items)=>(
-        <div className="Patients-List"  key={items._id}>
-          <div className="First-Patient">
-          <a style={{textDecoration:"none"}} href={items._id}>            
-            <div className="patient-id">
-              <img src="/img/image 2.png" alt="" />
+        {[...Details].reverse().map((items) => (
+          <div className="Patients-List" key={items._id}>
+            <div className="First-Patient">
+              <div className="patient-id">
+                <img src="/img/image 2.png" alt="" />
+                <div>
+                  <h4>{items.Name}</h4>
+                  <p>Client ID : {items.Clientid}</p>
+                </div>
+              </div>
               <div>
-                <h4>{items.Name}</h4>
-                <p>Client ID : {items.Clientid}</p>
+                <h4>Contact</h4>
+                <p>{items.Number}</p>
+              </div>
+              <div>
+                <h4>check up</h4>
+                <input maxLength={1} value={items.Treatment[0].Description} />
+              </div>
+              <div>
+                <h4>Date</h4>
+                <p>{items.Treatment[0].Date}</p>
+              </div>
+              <div>
+                <h4>Time</h4>
+                <p>{items.Treatment[0].Time}</p>
+              </div>
+              <div className="Options">
+                <Popup
+                  trigger={
+                    <div className="Options">
+                      <BsThreeDots />
+                    </div>
+                  }
+                  position="left"
+                >
+                  <div className="Options-For-Patient">
+                    <div className="Option-Flex">
+                    <div className="Delete">
+                      <MdDelete  className="Delete-Icon" />
+                      <h3>Delete Patient</h3>
+                    </div>
+                    <div className="Edit">
+                    <HiOutlinePencilAlt className="Edit-Icon"  />
+                      <h3>Edit Patient</h3>
+                    </div>
+                    </div>
+                  </div>
+                </Popup>
               </div>
             </div>
-            <div>
-              <h4>Contact</h4>
-              <p>{items.Number}</p>
-            </div>
-            <div>
-              <h4>check up</h4>
-              <input maxLength={1} value={items.Treatment[0].Description} />
-            </div>
-                        <div>
-              <h4>Date</h4>
-              <p>{items.Treatment[0].Date}</p>
-            </div>
-            <div>
-              <h4>Time</h4>
-              <p>{items.Treatment[0].Time}</p>
-            </div>
-            <div className="Options">
-            <Popup trigger={<div className="Options"><BsThreeDots /></div>} position="center">
-
-          <div className="Tooth-Description-Total">
-          
-
-              </div>
-        </Popup>
-        
-
-            
-            </div>
-            </a>
           </div>
-          
-        </div>
-       
-
-
-  
-        ))
-    }
-
-      
-
-
+        ))}
       </div>
     </>
   );

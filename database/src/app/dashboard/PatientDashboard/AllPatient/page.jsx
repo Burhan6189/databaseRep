f5e13114bdb9fbbeb5948e5c00b3e5226871db5a
Your Name <burhanshabbir6189@gmail.com>
2023-12-11 18:38:54 +0500

    burhan

--- database/src/app/dashboard/PatientDashboard/AllPatient/page.jsx
+++ database/src/app/dashboard/PatientDashboard/AllPatient/page.jsx
@@ -7,6 +7,7 @@ import { HiOutlinePencilAlt } from "react-icons/hi";
 import { IoMdArrowBack } from "react-icons/io";
 import Popup from "reactjs-popup";
 import Navbar from "@/app/components/Navbar/page";
+import { useSession } from "next-auth/react";
 
 const AllPatient = () => {
   const plusbtn = () => {
@@ -43,6 +44,9 @@ const AllPatient = () => {
     setRecord([...Details]);
   }, [Details]);
 
+
+  const { data: session } = useSession()
+  if (session?.user.name==="Burhan Shabbir") 
   return (
     <>
       <div className="All-Patient-BG">
@@ -127,7 +131,105 @@ const AllPatient = () => {
         </div>
       </div>
     </>
-  );
+  )
+                        
+else if (session?.user.name==="")
+
+  return(
+
+    <>
+    <div className="All-Patient-BG">
+      <Navbar />
+      <div className="All-Patient-Header">
+        <h2>All Patients</h2>
+        <div className="Appointment-Input">
+          <input type="text" placeholder="Search" onChange={Filter} />
+        </div>
+        <div className="Appointment-Button">
+          <h4>New Appointments</h4>
+          <button onClick={plusbtn}>+</button>
+        </div>
+      </div>
+      <div className="All-Patient">
+        {[...Record].reverse().map((items) => (
+          <div className="Patients-List" key={items._id}>
+            <div className="First-Patient">
+              <div className="patient-id">
+                <img src="/img/image 2.png" alt="" />
+                <div>
+                  <h4>{items.Name}</h4>
+                  <p>ID : {items.Clientid}</p>
+                </div>
+              </div>
+              <div>
+                <h4>Contact</h4>
+                <p>{items.Number}</p>
+              </div>
+              <div>
+                <h4>check up</h4>
+                <input
+                  disabled
+                  maxLength={1}
+                  value={items.Treatment[0].Description}
+                />
+              </div>
+              <div>
+                <h4>Date</h4>
+                <p>{items.Treatment[0].Date}</p>
+              </div>
+              <div>
+                <h4>Time</h4>
+                <p>{items.Treatment[0].Time}</p>
+              </div>
+              <div className="Options">
+                 <Popup
+                  trigger={
+                    <div className="Options">
+                      <BsThreeDots />
+                    </div>
+                  }
+                  position="left"
+                >
+                  <div className="Options-For-Patient">
+                    <div className="Option-Flex">
+                      <a
+                        href={
+                          "/dashboard/PatientDashboard/AllPatient/" +
+                          items._id
+                        }
+                      >
+                        <div className="Delete">
+                          <MdDelete className="Delete-Icon" />
+                          <button>Delete Patient</button>
+                        </div>
+                      </a>
+
+                      <a href={items._id}>
+                        <div className="Edit">
+                          <HiOutlinePencilAlt className="Edit-Icon" />
+                          <button>Edit Patient</button>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </Popup> 
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
+  )
+
+  return(
+    <>
+    <div> nothing is available</div>
+    </>
+  )
+
+
+
 };
 
 export default AllPatient;

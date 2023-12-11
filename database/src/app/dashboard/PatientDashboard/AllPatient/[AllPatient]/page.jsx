3f16c0b800804e5fd1f1e43fa50fc408532e655e
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-12-11 12:31:32 +0500

    umer

--- database/src/app/dashboard/PatientDashboard/AllPatient/[AllPatient]/page.jsx
+++ database/src/app/dashboard/PatientDashboard/AllPatient/[AllPatient]/page.jsx
@@ -1,28 +1,21 @@
 "use client";
 
-
 const AllPatient = async (props) => {
+  // id declaration
+  const id = props.params.AllPatient;
 
+  const data = await fetch("/api/patientdetails/" + id, {
+    method: "DELETE",
+    body: JSON.stringify({}),
+  });
 
-    // id declaration
-const id =    props.params.AllPatient;
-  
-const data = await fetch("/api/patientdetails/"+id,{
-method:"DELETE",
-body:JSON.stringify({})
-})
-
-if(data){
-
-  window.location.href=("/dashboard/PatientDashboard/AllPatient");  
-}
-else if(!data){
-  alert("Data does not Exist")
-}
-else{
-  alert(" Something is wrong")
-}
-
+  if (data) {
+    window.location.href = "/dashboard/PatientDashboard/AllPatient";
+  } else if (!data) {
+    alert("Data does not Exist");
+  } else {
+    alert(" Something is wrong");
+  }
 };
 
 export default AllPatient;

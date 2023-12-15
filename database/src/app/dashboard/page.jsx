dfc031d12e94773080b12ddc05956787c0a3cfa8
Your Name <burhanshabbir6189@gmail.com>
2023-12-15 19:03:34 +0500

    burhan

--- database/src/app/dashboard/page.jsx
+++ database/src/app/dashboard/page.jsx
@@ -12,6 +12,12 @@ const TodayAppointment = async() => {
   if (!session) {
     redirect('/signin?callbackUrl=/dashboard')
   }
+
+
+
+
+                              // this is for  Super Admin
+  else if(session.user.Role==="Super Admin" || session.user.Role==="User")
   return (
     <>
       <div className="Things-BG">
@@ -80,6 +86,73 @@ const TodayAppointment = async() => {
       </div>
     </>
   );
+
+
+
+
+
+
+
+
+                          // this is for user
+
+  else
+  return(
+    <>
+        <div className="Things-BG">
+<Navbar/>
+        <div className="Box-Flex">
+          <a href="/dashboard/Appoinments">
+            <div className="Box-02">
+              <div className="Icon-Circle">
+                <img src="/img/icon02.png" alt="" />
+              </div>
+              <div className="Box-Text">
+                <h3>Appointment</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </div>
+          </a>
+          <a
+            style={{ textDecoration: "none" }}
+            href="/dashboard/PatientDashboard/AllPatient"
+          >
+            <div className="Box-03">
+              <div className="Icon-Circle">
+                <img src="/img/icon01.png" alt="" />
+              </div>
+              <div className="Box-Text">
+                <h3>Old Patient</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="Bottom-Text">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s,
+          </p>
+          <h3>www.fhgroupoc.com</h3>
+        </div>
+      </div>
+    
+    
+    </>
+    )
+
+
+
+
 };
 
 export default TodayAppointment;

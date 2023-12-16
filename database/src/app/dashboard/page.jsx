04c8fcffe3bc988a8ccefb8539bd1e646a4ed2af
Your Name <burhanshabbir6189@gmail.com>
2023-12-16 16:36:53 +0500

    burhan

--- database/src/app/dashboard/page.jsx
+++ database/src/app/dashboard/page.jsx
@@ -7,7 +7,7 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 
 
-const TodayAppointment = async() => {
+const TodayAppointment = async () => {
   const session = await getServerSession(authOptions)
   if (!session) {
     redirect('/signin?callbackUrl=/dashboard')
@@ -15,32 +15,45 @@ const TodayAppointment = async() => {
 
 
 
-
-                              // this is for  Super Admin
-  else if(session.user.Role==="Super Admin" || session.user.Role==="User")
   return (
     <>
       <div className="Things-BG">
-<Navbar/>
+        <Navbar />
         <div className="Box-Flex">
-          <a
-            style={{ textDecoration: "none" }}
-            href="/dashboard/PatientDashboard/"
-          >
-            <div className="Box-01">
-              <div className="Icon-Circle">
-                <img src="/img/icon01.png" alt="" />
-              </div>
-              <div className="Box-Text">
-                <h3>New Patient</h3>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </p>
-              </div>
-            </div>
-          </a>
+
+
+
+          {/* this condition is for Super Admin and USer */}
+
+          {(session.user.Role === "Super Admin" || session.user.Role === "User") && (
+            <>
+              <a
+                style={{ textDecoration: "none" }}
+                href="/dashboard/PatientDashboard/"
+              >
+                <div className="Box-01">
+                  <div className="Icon-Circle">
+                    <img src="/img/icon01.png" alt="" />
+                  </div>
+                  <div className="Box-Text">
+                    <h3>New Patient</h3>
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s,
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </>
+          )
+
+          }
+
+
+          {/*  condition ends here */}
+
+
           <a href="/dashboard/Appoinments">
             <div className="Box-02">
               <div className="Icon-Circle">
@@ -85,70 +98,8 @@ const TodayAppointment = async() => {
         </div>
       </div>
     </>
-  );
+  )
 
-
-
-
-
-
-
-
-                          // this is for user
-
-  else
-  return(
-    <>
-        <div className="Things-BG">
-<Navbar/>
-        <div className="Box-Flex">
-          <a href="/dashboard/Appoinments">
-            <div className="Box-02">
-              <div className="Icon-Circle">
-                <img src="/img/icon02.png" alt="" />
-              </div>
-              <div className="Box-Text">
-                <h3>Appointment</h3>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </p>
-              </div>
-            </div>
-          </a>
-          <a
-            style={{ textDecoration: "none" }}
-            href="/dashboard/PatientDashboard/AllPatient"
-          >
-            <div className="Box-03">
-              <div className="Icon-Circle">
-                <img src="/img/icon01.png" alt="" />
-              </div>
-              <div className="Box-Text">
-                <h3>Old Patient</h3>
-                <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="Bottom-Text">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s,
-          </p>
-          <h3>www.fhgroupoc.com</h3>
-        </div>
-      </div>
-    
-    
-    </>
-    )
 
 
 

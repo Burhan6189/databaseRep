ec2b0359f0102fe2f3cc9b18cd58e9a6099cee48
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-12-18 17:04:10 +0500

    umer

--- database/src/app/components/GithubSignInButton.jsx
+++ database/src/app/components/GithubSignInButton.jsx
@@ -1,35 +1,33 @@
-'use client'
+"use client";
 
-import { redirect, useSearchParams } from 'next/navigation'
-import { signIn, useSession } from 'next-auth/react'
+import { redirect, useSearchParams } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
 
-import Button from './Button'
+import Button from "./Button";
 
 const GithubSignInButton = () => {
-  const searchParams = useSearchParams()
-  let callbackUrl = searchParams.get('callbackUrl')
-  const {data: session} =  useSession();
+  const searchParams = useSearchParams();
+  let callbackUrl = searchParams.get("callbackUrl");
+  const { data: session } = useSession();
 
-
-  if(session?.status ==="authenticated"){
+  if (session?.status === "authenticated") {
     router.replace("/dashboard");
+  } else if (!session) {
+    callbackUrl = "/dashboard";
   }
 
-  else if(!session){
-    callbackUrl="/dashboard";
-  }
-  
   return (
     <Button
-      className='login-buttons'
-      onClick={() => signIn('github', {callbackUrl}) }
+      className="login-buttons"
+      onClick={() => signIn("github", { callbackUrl })}
     >
-      
-<img src="https://github.githubassets.com/assets/apple-touch-icon-144x144-b882e354c005.png" alt="" />
-
+      <img
+        src="https://github.githubassets.com/assets/apple-touch-icon-144x144-b882e354c005.png"
+        alt=""
+      />
       Continue with Github
     </Button>
-  )
-}
+  );
+};
 
-export default GithubSignInButton
+export default GithubSignInButton;
--- database/src/app/components/GoogleSignInButton.jsx
+++ database/src/app/components/GoogleSignInButton.jsx
@@ -1,36 +1,33 @@
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation'
-import { signIn, useSession } from 'next-auth/react'
+import { useSearchParams } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
 
-import Button from './Button'
+import Button from "./Button";
 
 const GoogleSignInButton = () => {
-  const searchParams = useSearchParams()
-  let callbackUrl = searchParams.get('callbackUrl')
-  const {data: session} =  useSession();
+  const searchParams = useSearchParams();
+  let callbackUrl = searchParams.get("callbackUrl");
+  const { data: session } = useSession();
 
-  if(session?.status ==="authenticated"){
+  if (session?.status === "authenticated") {
     router.replace("/dashboard");
+  } else if (!session) {
+    callbackUrl = "/dashboard";
   }
-
-  else if(!session){
-    callbackUrl="/dashboard";
-  }
-
 
   return (
     <Button
-      className='login-buttons'
-      onClick={() => signIn('google', {callbackUrl})}
-      
+      className="login-buttons"
+      onClick={() => signIn("google", { callbackUrl })}
     >
-      
-<img src="https://www.google.com/images/hpp/ic_wahlberg_product_core_48.png8.png" alt="" />
-
+      <img
+        src="https://www.google.com/images/hpp/ic_wahlberg_product_core_48.png8.png"
+        alt=""
+      />
       Continue with Google
     </Button>
-  )
-}
+  );
+};
 
-export default GoogleSignInButton
+export default GoogleSignInButton;

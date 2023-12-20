9e3cadca6aa1d9d38033ccefb640c270cc4fd084
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-12-20 16:47:21 +0500

    umer

---
+++ database/src/app/dashboard/AiBot/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <h1>Ai Bot</h1>
+      <iframe
+        src="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_embed_html"
+        width="600"
+        height="450"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+};
+
+export default page;

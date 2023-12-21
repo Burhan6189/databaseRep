50840cfed6cb96011d43c3429aaa8fa464ba5bc6
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-12-21 14:05:59 +0500

    umer

--- database/src/app/dashboard/AiBot/page.jsx
+++ database/src/app/dashboard/AiBot/page.jsx
@@ -4,14 +4,6 @@ const page = () => {
   return (
     <>
       <h1>Ai Bot</h1>
-      <iframe
-        src="https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_embed_html"
-        width="600"
-        height="450"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
     </>
   );
 };

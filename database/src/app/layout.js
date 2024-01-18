ede3fc4f5e8d3fdd6e254c9eeab12e683b3c4403
Your Name <burhanshabbir6189@gmail.com>
2024-01-18 20:04:50 +0500

    burhan

--- database/src/app/layout.js
+++ database/src/app/layout.js
@@ -25,23 +25,28 @@ export default function RootLayout({ children }) {
             toastOptions={{
               success: {
                 style: {
-                  fontSize:"20px",
-                  width: "500px",
-                  height: "70px",
+                  fontSize:"22px",
+                  width: "600px",
+                  height: "100px",
+                  background:" rgb(174,197,238)",
+                  background:" radial-gradient(circle, rgba(174,197,238,1) 0%, rgba(167,233,148,0.7903536414565826) 100%)"
                  
                 },
               },
               error: {
                 style: {
-                  fontSize:"20px",
-                  width: "500px",
-                  height: "70px",
+                  fontSize:"22px",
+                  width: "600px",
+                  height: "100px",
+                  background:"rgb(162,135,135)",
+                  background:" linear-gradient(0deg, rgba(162,135,135,0.6166841736694677) 0%, rgba(164,180,190,0.8547794117647058) 100%)"
+
                  
                 },
               },
             }}
 
-            position='top-center' />
+            position='buttom-right' />
           
           {children}
           
